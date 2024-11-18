2d1899456ae234f1f8e29b13eb68db8e9ee4329a
Eduardo Gomez Nayawan JR. <enayawan@gbox.ncf.edu.ph>
2024-11-19 00:54:13 +0800

    yep

---
+++ src/pages/about.jsx
@@ -0,0 +1,19 @@
+import Layout from '../components/Layout';
+
+const About = () => {
+  return (
+    <Layout>
+    <div className="card shadow p-4" style={{ backgroundColor: 'lightgrey' }}>
+      <h2>About Me</h2>
+      <p> Hi! I'm Eduardo Nayawan, a Computer Science student with a passion for creating clean, functional, and user-friendly websites. 
+      Ever since I was introduced to the world of programming, I’ve been fascinated by the endless possibilities technology offers to solve real-world problems.</p>
+
+      <p> Growing up, I was always interested in how things worked—whether it was dismantling gadgets or diving into the world of software development.</p>
+      <p> I'm passionate about building websites that not only look great but also provide an intuitive and seamless user experience. In addition to my technical skills, I’m always eager to learn more about the latest trends in the tech world and explore innovative solutions.</p>
+
+    </div>
+    </Layout>
+  );
+};
+
+export default About;
